8a41f4c376689ee11e7776ca71ca3d668ca2611a
Eva <e.moritz.comcave@gmail.com>
2023-05-09 10:23:47 +0200

    bsp vor und nachname

--- script_01.js
+++ script_01.js
@@ -7,11 +7,28 @@
  *  Block-Kommentar
  * 
 */
+// Zeilenkommentar
+
+// zum Speichern: Strg + S
+// Kommentar ein/aus: Strg + #
+// Text in "" wird ausgegeben
+//ohne "" = Variable, deklarieren!
+// um Ausdruck zu markieren: Doppelklick
+// um ganze Zeile zu markieren: auf Zeilennr.
+// Variablen immer mit kleinem Buchstabe
+// weitere Worte ohne Leerzeichen Gross
 
 // Konsolen-Ausgaben
-// console.log("Hi");
-// console.log(firstName);
+// console.log("Hi"); -> funktioniert in Konsole
+// console.log(firstName); -> Fehler, weil:
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+let firstName; // Deklaration (Definition)
+firstName = "Max"; //Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName;
+familyName = "Mütze";
+console.log(familyName);
